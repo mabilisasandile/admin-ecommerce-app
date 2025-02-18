7c50d59bd97e346303b7ffaa38b40516feb2a1af
mabilisasandile <mabilisasandile@gmail.com>
2025-02-18 20:56:42 +0200

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,13 @@
+import logo from './logo.jpg'
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import parcel_icon from './parcel_icon.png'
+import upload_icon from './upload_icon.png'
+
+export const assets = {
+    logo,
+    add_icon,
+    order_icon,
+    parcel_icon,
+    upload_icon
+}
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Profile = ({token}) => {
+  return (
+    <div>Admin Profile</div>
+  )
+}
+
+export default Profile
